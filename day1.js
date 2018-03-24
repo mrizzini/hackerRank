6a9ef91e4113a95a818af9c2931254b5c05eacce
Matthew Rizzini <matthewrizzini@gmail.com>
2018-03-24 20:12:02 +0000

    day 1

---
+++ day1.js
@@ -0,0 +1,17 @@
+    // Declare second integer, double, and String variables.
+
+        var integer = readLine();
+        var double = readLine();
+        var String = readLine();
+
+        // console.log(integer + double + String);
+
+    // Read and save an integer, double, and String to your variables.
+
+    // Print the sum of both integer variables on a new line.
+        console.log(i + parseFloat(integer));
+    // Print the sum of the double variables on a new line.
+        console.log((parseFloat(double) + d).toFixed(1));
+    // Concatenate and print the String variables on a new line
+        console.log(s + String)
+    // The 's' variable above should be printed first.

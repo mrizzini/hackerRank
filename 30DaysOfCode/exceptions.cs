b85d753d4e42cdac369130c83222c5c3378c5798
Matthew Rizzini <matthewrizzini@gmail.com>
2018-07-21 14:40:23 +0000

    added solution for exceptions

---
+++ 30DaysOfCode/exceptions.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        string S = Console.ReadLine();
+    
+        try
+        {
+          Console.WriteLine(int.Parse(S));  
+        }
+        catch (Exception FormatException)
+        {
+            Console.WriteLine("Bad String");
+        }
+    }
+}
